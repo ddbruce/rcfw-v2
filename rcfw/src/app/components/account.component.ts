9c5abbd7c21fa736ac72a12077c0b7ccb5a6ad64
David-Sparky <sparkd@rpi.edu>
2018-04-18 12:38:37 -0400

    Server created

---
+++ rcfw/src/app/components/account.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Title }     from '@angular/platform-browser';
+
+
+@Component({
+  selector: 'my-account',
+  templateUrl: '../templates/account.template.html',
+  // styleUrls: ['../styles/app.component.css']
+})
+export class MyAccountComponent {
+
+
+}
